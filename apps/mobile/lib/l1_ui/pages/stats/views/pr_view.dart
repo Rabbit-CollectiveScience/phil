273b70be77cb17205bea98256c68ce39ce7897a2
NightRabbit <sirisilp.kongsilp@gmail.com>
2026-01-01 05:49:07 +0700

    create pr view remove trend view

--- apps/mobile/lib/l1_ui/pages/stats/views/pr_view.dart
+++ apps/mobile/lib/l1_ui/pages/stats/views/pr_view.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 import '../../../theme/app_colors.dart';
 
-class TrendView extends StatelessWidget {
-  const TrendView({super.key});
+class PRView extends StatelessWidget {
+  const PRView({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Text(
-        'Trend View - Coming Soon',
+        'PR View - Coming Soon',
         style: TextStyle(
           fontSize: 16,
           fontWeight: FontWeight.w600,
